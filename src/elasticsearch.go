e3676e2b9cb040d131eff7bb38b931a3fe411156
ssmith-sahnow <ssmith-sahnow@newrelic.com>
2018-11-27 14:29:19 -0800

    Added in the ability to pass in a ClusterEnvironment
    This will allow a user to configure the NRI ES integration to have the cluster name report with extra granularity about which enviroment it's in.
    By default this is disabled, and only makes a difference when a user passes in the arg.
    
    A example use case here is that a user has two ES clusters with the same name, but running in different enviroments.

--- src/elasticsearch.go
+++ src/elasticsearch.go
@@ -10,19 +10,20 @@ import (
 
 type argumentList struct {
 	sdkArgs.DefaultArgumentList
-	Hostname         string `default:"localhost" help:"Hostname or IP where Elasticsearch Node is running."`
-	LocalHostname    string `default:"localhost" help:"Hostname or IP of the Elasticsearch node from which to collect inventory."`
-	Port             int    `default:"9200" help:"Port on which Elasticsearch Node is listening."`
-	Username         string `default:"" help:"Username for accessing Elasticsearch Node"`
-	Password         string `default:"" help:"Password for the given user."`
-	UseSSL           bool   `default:"false" help:"Signals whether to use SSL or not. Certificate bundle must be supplied"`
-	CABundleFile     string `default:"" help:"Alternative Certificate Authority bundle file"`
-	CABundleDir      string `default:"" help:"Alternative Certificate Authority bundle directory"`
-	Timeout          int    `default:"30" help:"Timeout for an API call"`
-	ConfigPath       string `default:"/etc/elasticsearch/elasticsearch.yml" help:"Path to the ElasticSearch configuration .yml file."`
-	CollectIndices   bool   `default:"true" help:"Signals whether to collect indices metrics or not"`
-	CollectPrimaries bool   `default:"true" help:"Signals whether to collect primaries metrics or not"`
-	IndicesRegex     string `default:"" help:"JSON array of index names from which to collect metrics"`
+	Hostname           string `default:"localhost" help:"Hostname or IP where Elasticsearch Node is running."`
+	LocalHostname      string `default:"localhost" help:"Hostname or IP of the Elasticsearch node from which to collect inventory."`
+	ClusterEnvironment string `default:"" help:"A way to further specify which cluster we are gathering data for, example: 'staging'"`
+	Port               int    `default:"9200" help:"Port on which Elasticsearch Node is listening."`
+	Username           string `default:"" help:"Username for accessing Elasticsearch Node"`
+	Password           string `default:"" help:"Password for the given user."`
+	UseSSL             bool   `default:"false" help:"Signals whether to use SSL or not. Certificate bundle must be supplied"`
+	CABundleFile       string `default:"" help:"Alternative Certificate Authority bundle file"`
+	CABundleDir        string `default:"" help:"Alternative Certificate Authority bundle directory"`
+	Timeout            int    `default:"30" help:"Timeout for an API call"`
+	ConfigPath         string `default:"/etc/elasticsearch/elasticsearch.yml" help:"Path to the ElasticSearch configuration .yml file."`
+	CollectIndices     bool   `default:"true" help:"Signals whether to collect indices metrics or not"`
+	CollectPrimaries   bool   `default:"true" help:"Signals whether to collect primaries metrics or not"`
+	IndicesRegex       string `default:"" help:"JSON array of index names from which to collect metrics"`
 }
 
 const (
@@ -44,7 +45,7 @@ func main() {
 	logErrorAndExit(err)
 
 	if args.All() || args.Metrics {
-		populateMetrics(i, metricsClient)
+		populateMetrics(i, metricsClient, args.ClusterEnvironment)
 	}
 
 	// Create a client for inventory. Inventory needs to make REST calls against
